65780010893e2d29cf0ccc56993e520813bac203
Gui Coutinho <guic29@gmail.com>
2023-02-18 21:14:12 +0000

    Update question1.sql

--- The World of Pokémon/question1.sql
+++ The World of Pokémon/question1.sql
@@ -1,4 +1,4 @@
---Question 1) Which Pokémon Has the highest total points? And the lowest?
+-- Question 1) Which Pokémon Has the highest total points? And the lowest?
 
 SELECT
   pokedex_number,
@@ -23,4 +23,10 @@ WHERE
             FROM
               `pokemonproject2023.Pokemon.pokedex`
   )
+  
+-- Results:
+| pokedex_number | name | total | Gen |
+| -------------- | ---- | ----- | --- |
+| 746 | Wishiwashi | 175 | VII |
+| 890 | Eternatus Eternamax | 1125 | VIII|
   
